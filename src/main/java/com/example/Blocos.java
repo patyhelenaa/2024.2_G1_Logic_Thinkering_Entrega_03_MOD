b59acd2b7a3ba0c841304e9555ee70d472788c8c
Vinicius de Oliveira Santos <githubdovinicius@gmail.com>
2024-12-13 14:05:08 -0300

    Criando o bloco de clock e corrigindo texturas

--- src/main/java/com/example/Blocos.java
+++ src/main/java/com/example/Blocos.java
@@ -1,29 +1,21 @@
 package com.example;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.PillarBlock;
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
 
 public class Blocos {
 
-    public static final Block BLOCO_TESTE = RegistrarBlocos.registrarBloco(
-            "bloco_teste",
+    public static final Block CLOCK = RegistrarBlocos.registrarBloco(
+            "clock",
             Block::new,
             AbstractBlock.Settings.create().strength(4f).requiresTool()
     );
 
     public static void Inicializa() {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
-            entries.add(Blocos.BLOCO_TESTE);
+            entries.add(Blocos.CLOCK);
         });
     }
 }
