a68824689bd4c4701e2ac23fc1e7103916d8835b
Hunter104 <000plagueinc@gmail.com>
2025-01-03 17:31:29 -0300

    conserta codec errado para porta NOT

--- src/main/kotlin/com/logic_thinkering/digitalcircuits/NOTGate.kt
+++ src/main/kotlin/com/logic_thinkering/digitalcircuits/NOTGate.kt
@@ -4,8 +4,7 @@ import com.logic_thinkering.digitalcircuits.strategies.NOTStrategy
 import com.mojang.serialization.MapCodec
 import net.minecraft.block.AbstractBlock
 
-private val NOT_CODEC = AbstractBlock.createCodec(::NANDGate)
-
+private val NOT_CODEC = AbstractBlock.createCodec(::NOTGate)
 class NOTGate(settings: Settings) : AbstractLogicGate(settings, NOTStrategy()) {
-    override fun getCodec(): MapCodec<NANDGate> = NOT_CODEC
+    override fun getCodec(): MapCodec<NOTGate> = NOT_CODEC
 }
